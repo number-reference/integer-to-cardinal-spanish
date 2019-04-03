14ef8c992684ad96cba6e7ad70d89569098dec14
TimeEmit <LiamNorris1231853211@gmail.com>
2019-04-02 18:36:19 -0700

    Init

---
+++ index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const Cardinals = [
+  "Cero",
+  "Uno",
+  "Dos",
+  "Tres",
+  "Cuatro",
+  "Cinco",
+  "Seis",
+  "Siete",
+  "Ocho",
+  "Nueve",
+  "Diez",
+  "Once",
+  "Doce",
+  "Trece",
+  "Catorce",
+  "Quince",
+  "Dieciseis",
+  "Diecisiete",
+  "Dieciocho",
+  "Diecinueve",
+  "Veinte",
+  "Veintiuno",
+  "Veintidos",
+  "Veintitres",
+  "Veinticuatro",
+  "Veinticinco",
+  "Veintiseis",
+  "Veintisiete",
+  "Veintiocho",
+  "Veintinueve",
+  "Treinta",
+];
+
+const Hundreds = [
+  null,
+  "Ciento",  // Overidden when exactly one hundred
+  "Doscientos",
+  "Trescientos",
+  "Cuatrocientos",
+  "Quinientos",
+  "Seiscientos",
+  "Setecientos",
+  "Ochocientos",
+  "Novecientos",
+];
+
+const Thousands = [
+  "Mil",
+  "Un Millón",
+];
+
+const number_to_array = function(n) {
+
+};
